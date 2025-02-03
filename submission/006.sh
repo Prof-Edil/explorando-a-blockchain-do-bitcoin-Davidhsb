1619e5ea054f25615e60d27da6fdec8727f50d38
Davidhsb <davidherbertsb@gmail.com>
2025-02-03 02:21:03 -0300

    answer: question 6

--- submission/006.sh
+++ submission/006.sh
@@ -1,1 +1,21 @@
 # Which tx in block 257,343 spends the coinbase output of block 256,128?
+BLOCK_HASH_256128=$(bitcoin-cli getblockhash 256128)
+
+COINBASE_TX=$(bitcoin-cli getblock "$BLOCK_HASH_256128" | jq -r '.tx[0]')
+
+BLOCK_HASH_257343=$(bitcoin-cli getblockhash 257343)
+
+TX_LIST=$(bitcoin-cli getblock "$BLOCK_HASH_257343" | jq -r '.tx[]')
+
+
+for TX in $TX_LIST; do
+    RAW_TX=$(bitcoin-cli getrawtransaction "$TX" true)
+    MATCH=$(echo "$RAW_TX" | jq -r --arg COINBASE_TX "$COINBASE_TX" '.vin[] | select(.txid == $COINBASE_TX)')
+    
+    if [ ! -z "$MATCH" ]; then
+        echo "A transação que gasta a coinbase do bloco 256,128 no bloco 257,343 é: $TX"
+        exit 0
+    fi
+done
+
+echo "Nenhuma transação encontrada que gasta a coinbase do bloco 256,128 no bloco 257,343."
